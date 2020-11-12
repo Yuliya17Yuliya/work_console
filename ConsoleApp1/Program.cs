26e0e109a964d72d2927619ac33a0559698e5267
Yulia <yulia.malyutina@gmail.com>
2020-11-12 09:20:00 +0300

    Work console

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x;
+            Console.Write("Введите значение x:");
+            x = Convert.ToInt32(Console.ReadLine());
+            if (x > 1)
+                Console.WriteLine($"y={(1 - x) ^ 2}");
+            else
+            {
+                if (x < 1)
+                    Console.WriteLine($"y={x ^ 2}");
+                else
+                    Console.WriteLine($"y={0}");
+            }
+        }
+    }
+}
